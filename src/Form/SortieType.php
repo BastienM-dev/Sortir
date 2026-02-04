520ceca1d8a8327f6374db18a3a4024f2d83bbe1
Justine Rabin <justine.rabin2025@campus-eni.fr>
2026-02-04 14:41:16 +0100

    ajout form, fichier twig

---
+++ src/Form/SortieType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionsMax')
+            ->add('duree', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('urlPhoto', TextType::class, [
+                'required' => false,
+            ])
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer (En création)',
+            ])
+            ->add('publish', SubmitType::class, [
+                'label' => 'Publier (Ouverte)',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
