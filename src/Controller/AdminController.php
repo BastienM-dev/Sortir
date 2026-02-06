c39cb47f94f4e300f2b9835d66839aa305fa3702
Kev <letaileur.kevin@gmail.com>
2026-02-06 13:49:45 +0100

    villes and campus lists on admin dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
+use App\Repository\SiteRepository;
+use App\Repository\VilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,22 +21,49 @@ class AdminController extends AbstractController
     }
 
 
-    #[Route('/administration/campus', name: 'admin_campus')]
-    public function campus(): Response
+    #[Route('/administration/campus', name: 'admin_campus', methods: ['GET'])]
+    public function campus(SiteRepository $siteRepository, Request $request): Response
     {
-        return $this->render('admin/campus.html.twig');
+        $sites = $siteRepository->findBy([], ['nom' => 'ASC']);
+
+        if($request->query->count() !== 0){
+
+            $searchText = $request->query->get('search');
+
+            $qb = $siteRepository->createQueryBuilder('s')
+                ->andWhere('LOWER(s.nom) LIKE LOWER(:searchText)')
+                ->setParameter('searchText', '%' . $searchText . '%');
+
+            $sites = $qb->getQuery()->getResult();
+        }
+
+        return $this->render('admin/campus.html.twig', ['sites' => $sites]);
     }
 
     #[Route('/administration/villes', name: 'admin_villes')]
-    public function villes(): Response
+    public function villes(VilleRepository $villeRepository, Request $request): Response
     {
-        return $this->render('admin/villes.html.twig');
+        $villes = $villeRepository->findBy([], ['nom' => 'ASC']);
+
+        if($request->query->count() !== 0){
+
+            $searchText = $request->query->get('search');
+
+            $qb = $villeRepository->createQueryBuilder('v')
+                ->andWhere('LOWER(v.nom) LIKE LOWER(:searchText)')
+                ->setParameter('searchText', '%' . $searchText . '%');
+
+            $villes = $qb->getQuery()->getResult();
+        }
+
+        return $this->render('admin/villes.html.twig', ['villes' => $villes]);
     }
 
     #[Route('/administration/utilisateurs', name: 'admin_utilisateurs')]
-    public function utilisateurs(): Response
+    public function utilisateurs(ParticipantRepository $participantRepository): Response
     {
-        return $this->render('admin/utilisateurs.html.twig');
+        $participants = $participantRepository->findBy([], ['nom' => 'ASC']);
+        return $this->render('admin/utilisateurs.html.twig', ['participants' => $participants]);
     }
 
 }
